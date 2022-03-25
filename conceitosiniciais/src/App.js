c0c2c7fc81142044fccf6c3fcc4a968b49ef82a7
WILLY BORGES SILVA <willy.bsilva@senacsp.edu.br>
2022-03-24 21:00:30 -0300

    versão 1

---
+++ conceitosiniciais/src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <>
+        <h2>
+         SENAC CAMPINAS
+        </h2>
+    </>
+  );
+}
+
+export default App;
+        
+      
+        
+     

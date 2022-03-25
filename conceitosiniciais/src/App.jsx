8f0075d294fb52b4b4a004cc6505e04bd75fbc34
WILLY BORGES SILVA <willy.bsilva@senacsp.edu.br>
2022-03-24 22:08:34 -0300

    criar funcoes no componente e executalas, interpolando com chaves

--- conceitosiniciais/src/App.jsx
+++ conceitosiniciais/src/App.jsx
@@ -3,13 +3,25 @@ import './App.css';
 import Header from './Header'
 
 function App() {
+
+ function handleButtonClick(){
+   alert('Clicou no botão');
+ }
+
+ function sum(a,b){
+   return a+b;
+ }
+
+
   return (
     <>
         <Header title="Meu primeiro Component React" />
         <Header title="Um componente com props" />
         <h2>
-         SENAC CAMPINAS
+         SENAC CAMPINAS ...
+         <div>Retorno da soma: {sum(5,6)}</div>
         </h2>
+        <button onClick={handleButtonClick}>Click Me!</button>
     </>
   );
 }
